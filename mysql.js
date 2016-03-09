4b5630944f421f2d619d24a31eddc75064b99e68
Ketil Velle <ketil.velle@gmail.com>
2016-03-09 12:36:19 +0100

    mysql

--- mysql.js
+++ mysql.js
@@ -3,7 +3,8 @@ var mysql = require("mysql");
 var conn = mysql.createConnection({
   host:"localhost",
   user:"root",
-  password:"password"
+  password:"password",
+  database:"Tracking"
 });
 
 conn.connect(function(err) {
@@ -12,8 +13,26 @@ conn.connect(function(err) {
     return;
   }
   console.log("mysql connected");
+
+  var location =
+  {
+    userID: "1",
+    lat: "1.3578903",
+    lng: "103.12345"
+  }
+
+  conn.query('INSERT INTO Locations SET ?', location, function (err, res) {
+       if (err) throw err;
+       console.log("location ID", res.insertId);
+
+       conn.end(function (err) {
+           console.log("mysql disconnected")
+       })
+   });
 });
 
+/*
 conn.end(function(err) {
   console.log("mysql disconnected");
 });
+*/
